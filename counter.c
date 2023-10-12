ca6dcd0081814a929dee8c182fb19be268002550
Vortex <vortice.alessandro@gmail.com>
2023-10-12 20:36:18 +0200

    Added blink.c file and fixed mistakes in counter.c

--- counter.c
+++ counter.c
@@ -1,9 +1,9 @@
 /*
-8-bit counter with stm32f3discovery board LEDs
+8-bit counter with stm32f3discovery board LEDs.
 Given a 8-bit string we will use the following notation: LD6-LD8-LD10-LD9-LD7-LD5-LD3-LD4
 */
 
-#include "stm32f303xc.h"
+#include <stm32f303xc.h>
 
 int main()
 {
@@ -16,7 +16,8 @@ int main()
 		if ((GPIOA->IDR & 1) == 1) //Checks if the button is pressed
 		{
 			GPIOE->ODR += (1 << 8); //Adds 1 every the output registers
-			while ((GPIOA->IDR & 1) == 1) {} // Doesn't count while the button is pressed
+			while ((GPIOA->IDR & 1) == 1) {} //Doesn't count while the button is pressed
 		}
+	}
 	return 0;
 }
