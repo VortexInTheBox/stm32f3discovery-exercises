234999f12480b5c4cce57dce4d963060879836fb
Vortex <vortice.alessandro@gmail.com>
2023-10-18 08:46:44 +0200

    Setting the counter properly to 0 and adding small comments

--- blink.c
+++ blink.c
@@ -1,36 +1,39 @@
 /*
-Simple Blink program with the stm32f3discovery board LED.
+Simple Blink program with the stm32f3discovery board blue LED.
 It turns on the LD4 LED for 1s and turns it off for 1s repeadtly using the TIMER6
 */
 
 #include <stm32f303xc.h>
 
-int main(){
+int main()
+{
 	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
 	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
-	
+
 	GPIOE->MODER |= GPIO_MODER_MODER8_0;
-	
-	TIM6->CR1 |= TIM_CR1_CEN; //Enables the counter
-	TIM6->ARR = 8000; //The counter counts max to 8000
-	TIM6->PSC = 1000; //Rescalse the 8Mhz clock in order to make the counter fit in the TIM6_CNT
-	TIM6->CNT &= ~(0x00001111); //Sets the counter to 0
-	
+
+	TIM6->CR1 |= TIM_CR1_CEN;	// Enables the counter
+	TIM6->ARR = 8000;			// The counter counts max to 8000
+	TIM6->PSC = 1000;			// Rescalse the 8Mhz clock in order to make the counter fit in the TIM6_CNT
+	TIM6->CNT &= ~(0x0000FFFF); // Sets the counter to 0
+
 	short int flag = 0;
-	
-	while(1){
-		if((TIM6->SR & TIM_SR_UIF) == TIM_SR_UIF && !flag){
-			TIM6->SR &= ~TIM_SR_UIF; //Resets the flag raised by the counter
-			GPIOE->ODR |= GPIO_ODR_8; //Turns on the led
+
+	while (1)
+	{
+		if ((TIM6->SR & TIM_SR_UIF) == TIM_SR_UIF && !flag) //If the led is off
+		{
+			TIM6->SR &= ~TIM_SR_UIF;  // Resets the flag raised by the counter
+			GPIOE->ODR |= GPIO_ODR_8; // Turns on the led
 			flag = 1;
 		}
-		if((TIM6->SR & TIM_SR_UIF) == TIM_SR_UIF && flag){
+		if ((TIM6->SR & TIM_SR_UIF) == TIM_SR_UIF && flag) //If the led is on
+		{
 			TIM6->SR &= ~TIM_SR_UIF;
-			GPIOE->ODR &= ~GPIO_ODR_8; //Turns off the led
+			GPIOE->ODR &= ~GPIO_ODR_8; // Turns off the led
 			flag = 0;
 		}
-		
 	}
-	
+
 	return 0;
 }
